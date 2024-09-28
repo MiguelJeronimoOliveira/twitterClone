70e25abf4e69f867b1288a80d4c499ab18d2ae70
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2024-09-27 22:10:19 -0300

    Update: Entities
    Add: Getters and Setters

--- src/main/java/br/com/Entities/Post.java
+++ src/main/java/br/com/Entities/Post.java
@@ -1,11 +1,15 @@
 package br.com.Entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 public class Post {
@@ -17,6 +21,43 @@ public class Post {
 	private String conteudo;
 	
 	private String autor;
+	
+	@Temporal(TemporalType.TIME)
+	private Date tempoCriacao = new Date();
+	
+	
+	// getters e setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Date getTempoCriacao() {
+		return tempoCriacao;
+	}
+
+	public void setTempoCriacao(Date tempoCriacao) {
+		this.tempoCriacao = tempoCriacao;
+	}
 	
 	
 }
--- src/main/java/br/com/Entities/Usuario.java
+++ src/main/java/br/com/Entities/Usuario.java
@@ -21,4 +21,39 @@ public class Usuario {
 	
 	private String senha;
 	
+
+	//getters e setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
 }
