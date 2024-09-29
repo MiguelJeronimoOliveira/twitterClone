cefe970df5f9266cf6240bb57c0927dcf7c06c9f
MiguelJeronimoOliveira <164900366+MiguelJeronimoOliveira@users.noreply.github.com>
2024-09-29 20:15:13 -0300

    Create: UserSystem
    Add: sistema de criar e logar em contas

---
+++ src/main/java/br/com/Beans/LoginBean.java
@@ -0,0 +1,62 @@
+package br.com.Beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+import br.com.Entities.Usuario;
+import br.com.GenericDao.UsuarioDao;
+
+@ManagedBean(name = "loginBean")
+public class LoginBean {
+	
+	private Usuario usuario = new Usuario();
+	private UsuarioDao<Usuario> usuarioDao = new UsuarioDao<Usuario>();
+
+	public String Logar() {
+		Usuario usuarioLogar = usuarioDao.consultarUsuario(usuario.getUsername(), usuario.getEmail(), usuario.getSenha());
+		
+		if(usuarioLogar != null) {
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			externalContext.getSessionMap().put("usuarioLogado", usuarioLogar);
+		}
+		
+		
+		return "homePage?faces-redirect=true";
+	}
+	
+	public String Registrar() {
+		usuarioDao.Register(usuario);
+		usuario = new Usuario();
+		return "loginPage?faces-redirect=true";
+	}
+	
+	public String Logout() {
+		usuarioDao.Logout();
+		return "loginPage?faces-redirect=true";
+	}
+	
+	public String Redirect() {
+		return "registerPage?faces-redirect=true";
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public UsuarioDao<Usuario> getUsuarioDao() {
+		return usuarioDao;
+	}
+
+	public void setUsuarioDao(UsuarioDao<Usuario> usuarioDao) {
+		this.usuarioDao = usuarioDao;
+	}
+	
+	
+}
---
+++ src/main/java/br/com/GenericDao/UsuarioDao.java
@@ -0,0 +1,62 @@
+package br.com.GenericDao;
+
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.servlet.http.HttpSession;
+
+
+import br.com.Entities.Usuario;
+import br.com.Util.JPAUtil;
+
+public class UsuarioDao<U> {
+
+	protected EntityManager entityManager = JPAUtil.getEntityManager();
+	protected EntityTransaction entityTransaction = entityManager.getTransaction();
+
+	public void Register(U entidade) {
+		entityTransaction.begin();
+
+		entityManager.persist(entidade);
+		entityTransaction.commit();
+	}
+
+	public Usuario consultarUsuario(String username, String email, String senha) {
+		
+		Usuario usuario = null;
+		
+        usuario = (Usuario) entityManager.createQuery(
+                "select u from Usuario u where u.email = :email and u.senha = :senha", Usuario.class)
+                .setParameter("email", email)
+                .setParameter("senha", senha)
+                .getSingleResult();
+
+		return usuario;
+	}
+	
+	public Usuario GetUsuario() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		
+		Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+		return usuario;
+	}
+	
+	public String GetNomeUsuario() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		
+		Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+		
+		return usuario.getUsername();
+	}
+	
+	
+	public void Logout() {
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		
+		session.invalidate();
+	}
+
+}
